c14e72b35f96b298b653ee02362b7e42fc1f3648
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2012-08-21 15:45:43 +0200

    new message

--- src/main/java/pl/softmil/test/utils/waituntil/TimeoutException.java
+++ src/main/java/pl/softmil/test/utils/waituntil/TimeoutException.java
@@ -3,7 +3,7 @@ package pl.softmil.test.utils.waituntil;
 public class TimeoutException extends RuntimeException {
 
     public TimeoutException(long maxMillisToWait, Until<?> until) {
-        super("have been waiting for "+ until.toString() + " for " + maxMillisToWait +" millis" );
+        super("waiting for " + maxMillisToWait +" millis , until " + until.toString() );
     }
 
 }
